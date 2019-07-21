fc1734de5b2bc824cf1d8a7b0534e9ddd06abd89
Grigorii Horos <horosgrisa@gmail.com>
2019-07-21 14:19:52 +0300

    Fix deps

--- pr-eol.plugin.zsh
+++ pr-eol.plugin.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+DEPENDENCES_ZSH+=( zpm-zsh/colors )
+
+if command -v zpm >/dev/null; then
+  zpm zpm-zsh/colors
+fi
+
 if [[ "$CLICOLOR" = 1 ]]; then
   pr_eol="%{$bg[cyan]%}%{$fg_bold[white]%}⏎%{$reset_color%}"
 else
