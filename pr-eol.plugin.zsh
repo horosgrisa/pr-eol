a72969d663164341ee5238809856f75b8506c08b
Grigorii Horos <horosgrisa@gmail.com>
2019-08-09 01:03:22 +0300

    Fix colors

--- pr-eol.plugin.zsh
+++ pr-eol.plugin.zsh
@@ -6,8 +6,4 @@ if command -v zpm >/dev/null; then
   zpm zpm-zsh/colors
 fi
 
-if [[ "$CLICOLOR" = 1 ]]; then
-  pr_eol="%{$c_reset$c[blue]$c_bold%}⏎%{$c_reset%}"
-else
-  pr_eol="⏎"
-fi
+pr_eol="%{$c_reset$c[blue]$c_bold%}⏎%{$c_reset%}"
