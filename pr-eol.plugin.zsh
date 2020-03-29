3d5f68e34f1edaf4f40dafc3a93dad4a7185031b
Grigorii Horos <horosgrisa@gmail.com>
2020-03-29 03:20:39 +0300

    Optimize

--- pr-eol.plugin.zsh
+++ pr-eol.plugin.zsh
@@ -1,7 +1,3 @@
 #!/usr/bin/env zsh
 
-if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-
 pr_eol="%{${c[reset]}${c[blue]}${c[bold]}%}⏎%{${c[reset]}%}"
